a435a4284cc06bc0e7d2f062a7831331b8fdd4f1
arivicente <arivicenterj@gmail.com>
2019-09-26 15:47:00 -0300

    adição de todos os arquivos

---
+++ application/views/conteudo.php
@@ -0,0 +1,9 @@
+<p>
+
+Sunt commodo nisi ea sint in eiusmod magna aliqua ipsum duis duis ut deserunt elit. Duis elit velit qui tempor eu ipsum esse culpa exercitation ex irure pariatur fugiat. Commodo officia dolor veniam dolor exercitation velit esse velit deserunt nostrud. Sit deserunt ex sint eu commodo labore dolor non et quis sit.
+<br><br>
+Laboris labore adipisicing duis ad quis cupidatat enim. Labore labore ut velit sunt commodo et occaecat dolore cillum commodo ad. Culpa est velit in ex. Laborum aliquip minim do do laborum Lorem anim tempor. Proident veniam cupidatat nulla Lorem ullamco. Eiusmod dolore est fugiat elit irure fugiat excepteur aute culpa culpa proident consectetur aliqua.
+<br><br>
+Laborum veniam ea est in. Laboris nisi anim occaecat sint dolor commodo et voluptate anim eu nostrud ea mollit amet. Culpa laboris occaecat Lorem ullamco occaecat deserunt. Veniam in anim et culpa sunt labore cillum.
+
+</p>
---
+++ application/views/rodape.php
@@ -0,0 +1,6 @@
+
+    <hr>
+    <h3>Esse é o rodapé</h3>
+
+</body>
+</html>
